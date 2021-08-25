6b832a4664514236d6d7f9f18061ee7a258af7f0
rigobertoalag <a20150338@utem.edu.mx>
2021-08-25 12:14:08 -0500

    Checkpoint antes de usar jsonapi-serializer gem

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:show, :create, :update, :destroy]
       resources :tokens, only:[:create]
-      resources :products, only: [:show, :index]
+      resources :products
     end
   end
 end
-
-
-# Creando productos
-# Crear productos es un poco más complejo porque necesitaremos una configuración adicional. La estrategia que seguiremos es asignar el producto creado al usuario que pertenece al token JWT proporcionado en la cabecera HTTP Authorization.
--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -18,4 +18,57 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(self.response.body)
     assert_equal @product.title, json_response['title']
   end
+
+  test 'deberia crear un producto' do 
+    assert_difference('Product.count') do 
+      post api_v1_products_url,
+            params: { product: { title: @product.title, price: @product.price, published: @product.published } },
+            headers: { Authorization: JsonWebToken.encode(user_id: @product.user_id) },
+            as: :json
+    end
+    assert_response :created
+  end
+
+  test 'no deberia de crear producto sin un usuario' do
+    assert_no_difference('Product.count') do
+      post api_v1_products_url,
+            params: { product: { title: @product.title, price: @product.price, published: @product.published } },
+            as: :json
+    end
+    assert_response :forbidden
+  end
+
+  test 'deberia de actualizar el producto' do 
+    patch api_v1_product_url(@product),
+          params: { product: { title: @product.title } },
+          headers: { Authorization: JsonWebToken.encode(user_id: @product.user_id) },
+          as: :json
+    assert_response :success
+  end
+
+  test 'NO deberia de actualizar el producto ya que el usuario nos es el mismo' do
+    patch api_v1_product_url(@product),
+          params: { product: { title: @product.title } },
+          headers: { Authorization: JsonWebToken.encode(user_id: users(:two).id ) },
+          as: :json
+    assert_response :forbidden
+  end
+
+  test 'deberia de eliminar el producto' do 
+    assert_difference('Product.count', -1) do
+      delete api_v1_product_url(@product), 
+              headers: { Authorization: JsonWebToken.encode(user_id: @product.user_id) },
+              as: :json
+    end
+    assert_response :no_content
+  end
+
+  test 'NO deberia eliminar el producto el usuario no es el dueño' do
+    assert_no_difference('Product.count') do
+      delete api_v1_product_url(@product),
+              headers: { Authorization: JsonWebToken.encode(user_id: users(:two).id ) },
+              as: :json
+    end
+    assert_response :forbidden
+  end
 end
